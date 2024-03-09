a8ab5db2caf8f457dabd6607d1134f49f472fc4a
HanYingLee <han-ying.lee.1367@student.uu.se>
2024-03-09 19:42:30 +0000

    Add insertion error information

--- createdb_reddit.py
+++ createdb_reddit.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 import json
 import os
+
+# Initialization 
+insert_error_cnt =0
+insert_error_record = {}
 # Creating a client
-client = MongoClient()
+client = MongoClient('mongodb://localhost:27017/')
 # Create database
-db = client.get_database('reddit')
+db = client.get_database('test')
 # Create a document into a collection
 collection = db['collection']
 # set source folder
@@ -15,8 +19,19 @@ with open(file_path, 'r') as file:
         for obj in file:
                 # load the read text into json onject
                 reddit = json.loads(obj)
+                print(reddit)
                 # insert into db
                 result = collection.insert_one(reddit)
+                if result.acknowledged:
+                     print("Insertion successful. Inserted document ID:", result.inserted_id)
+                else:
+                     insert_error_cnt += 1
+                     err_msg = str(result.write_error)
+                     print("Insertion failed:", err_msg)
+                     insert_error_record[reddit['id']] = err_msg
 # Print total number of data in the collection
 document_count = collection.count_documents({})
 print("Number of documents:", document_count)
+# Print total errors
+print("Total insertion error: ", insert_error_cnt)
+print(insert_error_record)
